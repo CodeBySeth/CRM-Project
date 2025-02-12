4e24a89dd69c76e2bc33e587fc0bcfc5d271b6a0
CodingDude1234 <sethaivy@gmail.com>
2025-02-11 20:57:22 -0500

    Switched issues entity to sales to fit CRM and added comments to code.

--- project/src/main/java/com/crm/project/controllers/NewCustomerController.java
+++ project/src/main/java/com/crm/project/controllers/NewCustomerController.java
@@ -15,15 +15,18 @@ public class NewCustomerController {
     @Autowired
     private final NewCustomer userService;
 
+    // Constructor
     public NewCustomerController(NewCustomer userService) {
         this.userService = userService;
     }
 
+    // Get user from front-end and save to database
     @PostMapping("/save")
     public Customer createUser(@RequestBody Customer user) {
         return userService.saveUser(user);
     }
 
+    // Get all users from database
     @GetMapping
     public List<Customer> getUsers() {
         return userService.getAllUsers();
